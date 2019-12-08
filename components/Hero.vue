dbbc84f610bb3547ffb88cf073b1311c7098c297
Vic Eap <vice@synrgo.com>
2019-12-08 12:57:40 -0800

    Basic Hero image component

---
+++ components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-img
+    height="100vh"
+    src="bmw-918408_1280.jpg"
+    class="white--text"
+    >
+    </v-img>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
